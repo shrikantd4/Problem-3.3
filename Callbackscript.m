7eb31ff458df3c6a42dfc372fd52b0bf4e3c2d26
HP <HP@DESKTOP-BJ9Q993>
2020-12-18 13:09:17 +0530

    Callback script is added in project.

---
+++ Callbackscript.m
@@ -0,0 +1,12 @@
+% This is a callback script.
+
+% This is just demonstration, we can use callback for value initialization before model loading or we can do multiple operations.
+
+% We are not using this callback function to load initial values.
+% Because we are usingh data Dictionary.
+
+% This is a model of Anti-Lock Braking system.
+
+% Please go through the report for complete understanding.
+
+% Thank you.
